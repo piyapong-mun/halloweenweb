1b5a8767f01b9d4dfaf9c2e193ea69dc8655edfd
piyapung <piyapong.mun@student.mahidol.edu>
2025-10-30 20:22:32 +0700

    M

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  /* Allow CORS all origins */ 
   allowedDevOrigins: ['*', 'http://localhost:3000','192.168.71.1'],
+  eslint: {
+    // ✅ Disable ESLint during builds (Docker / CI / Production)
+    
+    ignoreDuringBuilds: true,
+    
+  },
 };
 
 export default nextConfig;
+
